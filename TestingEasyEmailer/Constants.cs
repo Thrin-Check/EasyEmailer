7a55611b1fac6ffc88034e7e6450bc5aaa730f85
Iamdatim <Timileyin4baze@gmail.com>
2023-11-20 10:47:27 +0100

    updates

---
+++ TestingEasyEmailer/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace TestingEasyEmailer
+{
+    public class Constants
+    {
+        public static string OutLookEmail = "testing9183@outlook.com";
+        public static string OutLookPassword = "Testing@2023";
+        public static string FacebookLink = "https://facebook.com/";
+        public static string TwitterLink = "https://twitter.com/";
+        public static string InstagramLink = "https://instagram.com/";
+        public static string LinkedInLink = "https://www.linkedin.com/";
+        public static string CompanyEmail = "support@trest.com";
+        public static string CompanyHexColorCode = "#900c3f";
+    }
+}
---
+++ TestingEasyEmailer/Controllers/EmailSenderController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ThrinCheck.EasyEmailer;
+
+namespace TestingEasyEmailer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailSenderController : ControllerBase
+    {
+        private readonly EasyEmailer _easyEmailer;
+        public EmailSenderController(EasyEmailer easyEmailer)
+        {
+            _easyEmailer = easyEmailer;
+        }
+
+
+        [HttpPost("WithCode")]
+        public async Task<IActionResult> SendEmailConfirmationCode(string receiverEmail)
+        {
+            try
+            {
+                var emailBody = _easyEmailer.PrepareEmailVerificationWithCode("345867", Constants.FacebookLink, Constants.LinkedInLink, Constants.TwitterLink, Constants.InstagramLink, Constants.CompanyEmail, "08100220011", Constants.CompanyHexColorCode);
+
+                var sendMail = await _easyEmailer.SendEmailAsync(Constants.OutLookEmail, Constants.OutLookPassword, receiverEmail, "Email Verification with code", emailBody);
+                if (sendMail)
+                {
+                    return Ok("Email Sent Successfully");
+                }
+                return BadRequest("Fail to send");
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("WithLink")]
+        public async Task<IActionResult> SendEmailConfirmationLink(string receiverEmail)
+        {
+            try
+            {
+                var emailBody = _easyEmailer.PrepareEmailVerificationWithLink("https://trest.com/verify", Constants.FacebookLink, Constants.LinkedInLink, Constants.TwitterLink, Constants.InstagramLink, Constants.CompanyEmail, "08100220011", Constants.CompanyHexColorCode);
+
+                var sendMail = await _easyEmailer.SendEmailAsync(Constants.OutLookEmail, Constants.OutLookPassword, receiverEmail, "Email Verification with Link", emailBody);
+                if (sendMail)
+                {
+                    return Ok("Email Sent Successfully");
+                }
+                return BadRequest("Fail to send");
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("PasswordReset")]
+        public async Task<IActionResult> SendPasswordResetMail(string receiverEmail)
+        {
+            try
+            {
+                var emailBody = _easyEmailer.PreparePasswordResetEmail("https://trest.com/password-reset", Constants.FacebookLink, Constants.LinkedInLink, Constants.TwitterLink, Constants.InstagramLink, Constants.CompanyEmail, Constants.CompanyHexColorCode);
+
+                var sendMail = await _easyEmailer.SendEmailAsync(Constants.OutLookEmail, Constants.OutLookPassword, receiverEmail, "Password Reset", emailBody);
+                if (sendMail)
+                {
+                    return Ok("Email Sent Successfully");
+                }
+                return BadRequest("Fail to send");
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
